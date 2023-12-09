9b8befc73a2e430406eda7fe1192ba58c8849eec
ramanjk <ramanjaneyulu476@gmail.com>
2023-12-09 19:19:21 +0530

    updated build.sh file correct deployment file

--- CD/build.sh
+++ CD/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BUILD_NUMBER=${BUILD_NUMBER}
-sed -i "s/mydevopsacr1.azurecr.io\/cicd:.*/mydevopsacr1.azurecr.io\/cicd:${BUILD_NUMBER}/g" CD/deployment.yml
+sed -i "s/mydevopsacr1.azurecr.io\/cicd:.*/mydevopsacr1.azurecr.io\/cicd:${BUILD_NUMBER}/g" CD/deployment.yaml
